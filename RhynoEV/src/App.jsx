9422c8dfdad57bd15fc71619ac4b9a9771033340
sukhdeep <sukhdeepkumar918@gmail.com>
2024-07-20 22:54:50 +0530

    second

---
+++ RhynoEV/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <main className='bg-black h-12'>
+        <Navbar></Navbar>
+    </main>
+  )
+}
+
+export default App
--- RhynoEV/src/components/Navbar.jsx
+++ RhynoEV/src/components/Navbar.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import {Link} from "@chakra-ui/react"
 const Navbar = () => {
-  return (
-    <nav className="bg-darkgrey p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-white font-poppins text-lg">Rhyno</Link>
-        <div className="space-x-4">
-          <Link to="/" className="text-white hover:text-yellow">Home</Link>
-          <Link to="/aboutus" className="text-white hover:text-yellow">About Us</Link>
-          <Link to="/products/se03lite" className="text-white hover:text-yellow">Products</Link>
-          <Link to="/contactus" className="text-white hover:text-yellow">Contact Us</Link>
-          <Link to="/prebook" className="text-white hover:text-yellow">Pre-Book Now</Link>
-        </div>
-      </div>
-    </nav>
+  const Menues =[
+    "Home","Aboutus","Products","Contactus","Pre-booknow","instagram", "linkedin"
+    
+  ]
+  return ( 
+    <header >
+        <nav>
+          <div >
+            
+          </div>
+        </nav>
+    </header>
   );
 };
 
