8c05abc4327860882b1686b127dd9b6f2c721fe0
sukhdeep <sukhdeepkumar918@gmail.com>
2024-07-21 22:56:45 +0530

    4th

--- RhynoEV/src/components/Navbar.jsx
+++ RhynoEV/src/components/Navbar.jsx
@@ -4,29 +4,31 @@ import { FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 const Navbar = () => {
   const Menues = [
-    "Home", "Aboutus", "Products", "Contactus", "Pre-booknow", "Instagram", "Linkedin"
+    "Home", "About Us", "Products", "Contact Us", "Pre-booknow", "Instagram", "Linkedin"
   ];
 
   return (
-    <header className="bg-black h-12 justify-between w-full">
+    <header className="bg-zinc-950 h-12 justify-between w-full">
       <nav>
-        <div className="flex flex-1 grow space-x-36 justify-center">
+        <div className="flex flex-1 grow space-x-32 justify-center">
           {Menues.map((menu) => (
-            <div key={menu} className="text-yellow mt-2 cursor-pointer">
+            <div key={menu} className="text-white mt-2 cursor-pointer">
               {menu === "Instagram" ? (
-                <div>  
-                  <p className="text-gray hover:text-white text-sm">Instagram</p>
-                  <Link href="https://www.instagram.com" isExternal>
+                
+                  
+                  <Link href="https://www.instagram.com" isExternal className='flex'>
+                    <p className="text-gray hover:text-white text-sm mr-1">Instagram</p>
                     <FaInstagram className="text-gray hover:text-pink-950 text-sm" size="24px" />
                   </Link>
-                </div>
+                
               ) : menu === "Linkedin" ? (
-                <div>
-                  <p className="text-gray hover:text-white">Linkdin</p>
-                  <Link href="https://www.linkedin.com" isExternal>
+               
+                 
+                  <Link href="https://www.linkedin.com" isExternal className='flex'>
+                     <p className="text-gray hover:text-white mr-1">Linkdin</p>
                      <FaLinkedin className="text-gray hover:text-blue-700" size="24px" />
                   </Link>
-                </div>
+                
               ) : (
                 <p className="text-gray hover:text-white">{menu}</p>
               )}
--- RhynoEV/src/components/Section.jsx
+++ RhynoEV/src/components/Section.jsx
@@ -9,17 +9,46 @@ const Section = () => {
         duration:1.5,
         y:-100
      })
-    })
+
+     const images = gsap.utils.toArray('.carousel-image');
+     gsap.to(images, {
+       xPercent: -100 * (images.length - 1),
+       ease: 'none',
+       duration: 8,
+       scrollTrigger: {
+         trigger: '.carousel',
+         pin: true,
+         scrub: 1,
+         end: `+=${window.innerWidth * images.length}`
+       },
+       repeat: -1 // This will make the animation repeat infinitely
+     });
+    },[]);
+
+    
+    
   return (
-    <section className='bg-slate-950 w-full'>
-         <div className=' h-96 '>
+    <section className='bg-slate-950 w-full items-center'>
+         <div className='h-auto' >
             <h1 className='justify-center text-center text-white text-3xl'>
                 <p className='pt-40 h1rhyno '  id='h1'>
                   Let's Elevate Your Ride Experience With Rhyno– 
                 <br/>Where Superiority Meets Style.
                 </p>
             </h1>
+ 
+        <div className='carousel flex overflow-hidden relative mt-10'>
+          <img src='/bike.png' alt='Bike 1' className='carousel-image w-1/3 bike-image' />
+          <img src='/blue.png' alt='Bike 2' className='carousel-image w-1/3 bike-image' />
+          <img src='/black.png' alt='Bike 3' className='carousel-image w-1/3 bike-image' />
         </div>
+
+        <button className='prebook-button'>
+          Pre-book
+        </button>
+        </div>
+
+        
     </section>
   )
 }
